2f8f66741bcfe635b69d07453fb0793166aa259b
GyLevent <gyurkol@kkszki.hu>
2023-10-05 08:05:34 +0200

    API: Connect Created

---
+++ Jegyek/Connect.cs
@@ -0,0 +1,23 @@
+﻿using MySqlConnector;
+
+namespace Jegyek
+{
+    public class Connect
+    {
+        public MySqlConnection Connection { get; set; }
+        private string Host;
+        private string DbName;
+        private string UserName;
+        private string Password;
+        private string ConnectionString;
+        public Connect()
+        {
+            Host = "localhost";
+            DbName = "db_user";
+            UserName = "root";
+            Password = "rootpwd";
+            ConnectionString = $"server={Host};database={DbName};user={UserName};password={Password}";
+            Connection = new MySqlConnection(ConnectionString);
+        }
+    }
+}
---
+++ Jegyek/DTOS.cs
@@ -0,0 +1,8 @@
+﻿namespace Jegyek
+{
+    public class DTOS
+    {
+        public record CreateMark(Guid id,int Mark,string Description,string CreatedTime);
+        public record UpdateMark(int Mark,string Description);
+    }
+}
