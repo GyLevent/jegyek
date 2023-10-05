dcfec07b72d39eb1f8d5b02ca3225c728b084c38
GyLevent <gyurkol@kkszki.hu>
2023-10-05 07:52:25 +0200

    API: Models edit

--- Jegyek/Models/Jegyek.cs
+++ Jegyek/Models/Jegyek.cs
@@ -1,6 +1,12 @@
 ﻿namespace Jegyek.Models
 {
-    public class Class
+    public class Jegyek
     {
+        public Guid Azon { get; set; }
+        public int jegy { get; set; }
+        public string leiras { get; set; }
+        public string letrehozas { get; set; }
+
+
     }
 }
