7738649a17cb670af07e13edc37bed02d6f46829
GyLevent <gyurkol@kkszki.hu>
2023-10-05 08:16:42 +0200

    API: Controller Created

---
+++ Jegyek/Controllers/MarkController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using Jegyek.Models;
+using static Jegyek.DTOS;
+
+namespace Jegyek.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarkController : ControllerBase
+    {
+        Connect connect = new Connect();
+        [HttpGet]
+
+        [HttpGet("{Id}")]
+        [HttpPost]
+        [HttpPut]
+        [HttpDelete]
+
+
+
+
+    }
+}
