2d9720f2e404f8db64c0eaafabdfafce3500a693
GyLevent <gyurkol@kkszki.hu>
2023-10-05 07:59:23 +0200

    API: DTOS Created

--- Jegyek/Models/Jegyek.cs
+++ Jegyek/Models/Jegyek.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
         public int Mark { get; set; }
         public string Description { get; set; }
-        public string Created { get; set; }
+        public string CreatedTime { get; set; }
 
 
     }
